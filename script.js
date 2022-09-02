04966b26667b34612c3aa109ed9b905f2113cd49
Enrico Casaquia Fernandes <101847703+enricocasaquia@users.noreply.github.com>
2022-09-02 00:49:28 -0300

    update js
    mudanças no código e comentários

--- script.js
+++ script.js
@@ -141,23 +141,25 @@ var carta14 = {
 //array com todas as cartas
 var cartas = [carta1, carta2, carta3, carta4, carta5, carta6, carta7, carta8, carta9, carta10, carta11, carta12, carta13, carta14];
 //inicialização das variáveis como vazias
-var cartaMáquina = 0
+var cartaMaquina = 0
 var cartaJogador = 0
   
 //função atribuida ao botão, que sorteia as carta para o jogador e a máquina
 function sortearCarta() {
   
+    //habilita a caixa de texto
+        document.getElementById("resultado").hidden = false;
 
-    //definição da cartaMáquina como aleatória entre as 3 opções 
-        var númeroCartaMáquina = parseInt(Math.random() * 14);
-        cartaMáquina = cartas[númeroCartaMáquina];
+    //definição da cartaMáquina como aleatória entre as 14 opções 
+        var numeroCartaMaquina = parseInt(Math.random() * 14);
+        cartaMaquina = cartas[numeroCartaMaquina];
     
     //definição da cartaJogador como aleatória, com exclusão da possibilidade de ser igual a cartaMáquina usando o while
-        var númeroCartaJogador = parseInt(Math.random() * 14);
-        while (númeroCartaMáquina == númeroCartaJogador) {
-            númeroCartaJogador = parseInt(Math.random() * 14);
+        var numeroCartaJogador = parseInt(Math.random() * 14);
+        while (numeroCartaMaquina == numeroCartaJogador) {
+            numeroCartaJogador = parseInt(Math.random() * 14);
         }
-        cartaJogador = cartas[númeroCartaJogador]; 
+        cartaJogador = cartas[numeroCartaJogador]; 
     
 
     //desabilita o botão de sortear após seu uso e habilita o botão de jogar 
@@ -167,24 +169,9 @@ function sortearCarta() {
     //exibe texto indicando o que o usuário escolha um dos atributos  
         document.getElementById("resultado").innerHTML = "Escolha seu atributo!!";
 
-    //executa a função de exibir a carta sorteada do jogador
+    //executa a função de exibir a carta sorteada do jogador (função criada no final do código)
         exibirCartaJogador();
   }
-     
-//função para exibir as opções de atributo a serem escolhidas nas cartas
-function exibirOpções() {
-
-    //variável iniciada para os valores da tag <h3></h3> com id="opcoes"
-        var opçõesDeAtributos = document.getElementById("opcoes");
-    //variável iniciada para o preenchimento com os atributos   
-        var opçõesDeAtributosTexto = "";
-    //for para a variável opçõesDeAtributosTexto
-        for (var atributo in cartaJogador.atributos) {
-            opçõesDeAtributosTexto += "<input type='radio' name='atributo' value='" + atributo + "'>" + atributo;
-        }
-    //preenchimento do HTML com os atributos
-    opçõesDeAtributos.innerHTML = opçõesDeAtributosTexto;  
-}
   
 //função que obtém o atributo selecionado pelo jogador e prepara para jogar
 function obtemAtributoSelecionado() {
@@ -203,47 +190,45 @@ function obtemAtributoSelecionado() {
 //função que executa a jogada após escolher os atributos
 function jogar() {
 
-    //variável que armazena o valor dos atributos definidos na função "obtemAtributoSelecionado"
+    //variável que armazena o valor dos atributos retornados na função "obtemAtributoSelecionado"
         var atributoSelecionado = obtemAtributoSelecionado();
     //variáveis que armazenam o valor dos atributos definidos especificamente para elas (uma para a carta do jogador e outra para a carta da máquina)
         var valorCartaJogador = cartaJogador.atributos[atributoSelecionado];
-        var valorCartaMáquina = cartaMáquina.atributos[atributoSelecionado];
-    //variável que aloca o espaço da tag <h2></h2> com id="resultado"
+        var valorCartaMaquina = cartaMaquina.atributos[atributoSelecionado];
+    //variável que aloca o espaço da tag <h2> com id="resultado"
         var atributoResultado = document.getElementById("resultado");
     //condicional caso o valor do atributo escolhido pelo jogador for maior que o seu respectivo atributo na carta da máquina
-        if (valorCartaJogador > valorCartaMáquina) {
+        if (valorCartaJogador > valorCartaMaquina) {
             atributoResultado.innerHTML = "Você venceu! O valor do seu atributo é maior que do adversário";
             document.getElementById("btnJogarNovamente").disabled = false;
         }
     //condicional caso o valor do atributo escolhido pelo jogador for menor que o seu respectivo atributo na carta da máquina     
-        else if (valorCartaJogador < valorCartaMáquina) {
+        else if (valorCartaJogador < valorCartaMaquina) {
             atributoResultado.innerHTML = "Você perdeu! O valor do seu atributo é menor que do adversário";
             document.getElementById("btnJogarNovamente").disabled = false;
         } 
     //condicional caso o valor do atributo escolhido pelo jogador for igual ao seu respectivo atributo na carta da máquina    
-        else if (valorCartaJogador = valorCartaMáquina) {
+        else if (valorCartaJogador = valorCartaMaquina) {
             atributoResultado.innerHTML = "Empate! O valor do seu atributo é igual ao de seu adversário";
             document.getElementById("btnJogarNovamente").disabled = false;
         }
     //condicional caso o jogador não selecione nenhum atributo
-        else if (valorCartaJogador == valorCartaMáquina) {
+        else if (valorCartaJogador == valorCartaMaquina) {
             alert("Selecione um atributo!!");
             return false;
         }
     //comando para desabilitar o botão "Jogar" após o usuário clicar no mesmo
         document.getElementById("btnJogar").disabled = true;
     //executa a função de exibir a carta sorteada da máquina
-        exibirCartaMáquina();
-    //apaga a frase: "Escolha seu atributo!!" da tela
-        document.getElementById("escolha").innerHTML = "";
+        exibirCartaMaquina();
 }
   
 //função que exibe a carta sorteada do jogador
 function exibirCartaJogador() {
         
-    //variável que aloca o espaço da tag <div></div> com id="carta-jogador" 
+    //variável que aloca o espaço da tag <div> com id="carta-jogador" 
         var divCartaJogador = document.getElementById("carta-jogador");
-    //alteração do imagem de fundo da div no CSS
+    //alteração do imagem de fundo da div no CSS pelo JS
         divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`;
     //variável para armazenar a imagem da moldura da carta
         var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style="width: inherit; height: inherit; position: absolute;"/>';
@@ -252,33 +237,33 @@ function exibirCartaJogador() {
     //variável para armazenar o valor dos atributos
         var tagHTML = "<div id='opcoes' class='carta-status'>";
     //variável iniciada para o preenchimento com os atributos   
-        var opçõesDeAtributosTexto = "";
-    //for para a variável opçõesDeAtributosTexto (gera os inputs com os atributos com quebra de linha entre si)  
+        var opcoesDeAtributosTexto = "";
+    //for para a variável opçõesDeAtributosTexto (gera os inputs com os atributos e seu valores, com quebra de linha entre si)  
         for (var atributo in cartaJogador.atributos) {
-            opçõesDeAtributosTexto += "<input type='radio' name='atributo' value='" + atributo + "'/>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>";
+            opcoesDeAtributosTexto += "<input type='radio' name='atributo' value='" + atributo + "'/>" + atributo + " " + cartaJogador.atributos[atributo] + "<br>";
         }
     //escreve na div as variáveis escolhidas (moldura;nomeDaCartaJogador;tagHTML e opçõesDeAtributosTexto)
-        divCartaJogador.innerHTML = moldura + nomeDaCartaJogador + tagHTML + opçõesDeAtributosTexto + "</div>";
+        divCartaJogador.innerHTML = moldura + nomeDaCartaJogador + tagHTML + opcoesDeAtributosTexto + "</div>";
 }
   
-function exibirCartaMáquina() {
+function exibirCartaMaquina() {
         
-    //variável que aloca o espaço da tag <div></div> com id="carta-maquina"     
-        var divCartaMáquina = document.getElementById("carta-maquina");
-    //alteração do imagem de fundo da div no CSS
-        divCartaMáquina.style.backgroundImage = `url(${cartaMáquina.imagem})`;
+    //variável que aloca o espaço da tag <div> com id="carta-maquina"     
+        var divCartaMaquina = document.getElementById("carta-maquina");
+    //alteração do imagem de fundo da div no CSS pelo JS
+        divCartaMaquina.style.backgroundImage = `url(${cartaMaquina.imagem})`;
     //variável para armazenar a imagem da moldura da carta
         var moldura ='<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent.png" style=" width: inherit; height: inherit; position: absolute;"/>';
     //variável para armazenar o nome da carta sorteada para a máquina
-        var nomeDaCartaMáquina = `<p class="carta-subtitle">${cartaMáquina.nome}</p>`;
+        var nomeDaCartaMaquina = `<p class="carta-subtitle">${cartaMaquina.nome}</p>`;
     //variável para armazenar o valor dos atributos    
         var tagHTML = "<div id='opcoes' class='carta-status'>";
     //variável iniciada para o preenchimento com os atributos
-        var opçõesDeAtributosTexto = "";
-    //for para a variável opçõesDeAtributosTexto (gera os parágrafos com os atributos da carta da máquina)
-        for (var atributo in cartaMáquina.atributos) {
-            opçõesDeAtributosTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMáquina.atributos[atributo] + "</p>";
+        var opcoesDeAtributosTexto = "";
+    //for para a variável opçõesDeAtributosTexto (gera os parágrafos com os atributos e valores da carta da máquina)
+        for (var atributo in cartaMaquina.atributos) {
+            opcoesDeAtributosTexto += "<p type='text' name='atributo' value='" + atributo + "'>" + atributo + " " + cartaMaquina.atributos[atributo] + "</p>";
         }
     //escreve na div as variáveis escolhidas (moldura;nomeDaCartaMáquina;tagHTML e opçõesDeAtributosTexto)    
-        divCartaMáquina.innerHTML = moldura + nomeDaCartaMáquina + tagHTML + opçõesDeAtributosTexto + "</div>";
+        divCartaMaquina.innerHTML = moldura + nomeDaCartaMaquina + tagHTML + opcoesDeAtributosTexto + "</div>";
 }
